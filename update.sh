226d80d5d4b4f10369659c0a6e5d97db3de69092
Rafal Zajac <rzajac@gmail.com>
2015-05-21 17:02:11 +0200

    gh-page

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git checkout master -- .gitignore
+
+git co master -- demo/demo.css
+git co master -- demo/index.html
+git co master -- dist/rzslider.css
+git co master -- dist/rzslider.css
+git co master -- rzslider.js
+
+mv demo/demo.css demo.css
+mv demo/index.html index.html
+mv dist/rzslider.css rzslider.css
+
+cp bower_components/angular/angular.min.js angular.min.js
+
+rm -rf demo
+rm -rf dist
