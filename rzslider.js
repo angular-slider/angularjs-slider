f2d86deabf73932b4ddd3e725317c498b1fec9cf
Valentin Hervieu <valentin.hervieu@milanamos.com>
2015-08-03 11:47:41 +0200

    Remove useless condition in updateLowHandle. Closes #77

--- rzslider.js
+++ rzslider.js
@@ -620,7 +620,7 @@ function throttle(func, wait, options) {
     {
       var delta = Math.abs(this.minH.rzsl - newOffset);
 
-      if(this.minLab.rzsv && delta <= 0 && delta < 1) { return; }
+      if(this.minLab.rzsv && delta < 1) { return; }
 
       this.setLeft(this.minH, newOffset);
       this.translateFn(this.scope.rzSliderModel, this.minLab);
