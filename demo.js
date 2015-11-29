405fabcd326d7c2fefd137a7d62478cb99ed1255
Valentin Hervieu <enforce46@hotmail.fr>
2015-11-29 11:25:52 +0100

    Update demo from master

--- demo.js
+++ demo.js
@@ -118,6 +118,66 @@ app.controller('MainCtrl', function($scope, $rootScope, $timeout, $modal) {
     }
   };
 
+  //Vertical sliders
+  $scope.verticalSlider1 = {
+    value: 0,
+    options: {
+      floor: 0,
+      ceil: 10,
+      vertical: true
+    }
+  };
+  $scope.verticalSlider2 = {
+    minValue: 20,
+    maxValue: 80,
+    options: {
+      floor: 0,
+      ceil: 100,
+      vertical: true
+    }
+  };
+  $scope.verticalSlider3 = {
+    value: 5,
+    options: {
+      floor: 0,
+      ceil: 10,
+      vertical: true,
+      showTicks: true
+    }
+  };
+  $scope.verticalSlider4 = {
+    minValue: 1,
+    maxValue: 5,
+    options: {
+      floor: 0,
+      ceil: 6,
+      vertical: true,
+      showTicksValues: true
+    }
+  };
+  $scope.verticalSlider5 = {
+    value: 50,
+    options: {
+      floor: 0,
+      ceil: 100,
+      vertical: true,
+      showSelectionBar: true
+    }
+  };
+  $scope.verticalSlider6 = {
+    value: 6,
+    options: {
+      floor: 0,
+      ceil: 6,
+      vertical: true,
+      showSelectionBar: true,
+      showTicksValues: true,
+      ticksValuesTooltip: function(v) {
+        return 'Tooltip for ' + v;
+      }
+    }
+  };
+
   //Read-only slider
   $scope.read_only_slider = {
     value: 50,
