86c4b242c0149bff8dda70f5a40fd825a117c663
Valentin Hervieu <valentin.hervieu@milanamos.com>
2015-12-21 10:30:35 +0100

    chore(CI): Add .travis.yml file (for real this time)

--- dist/rzslider.js
+++ dist/rzslider.js
@@ -1,7 +1,7 @@
 /*! angularjs-slider - v2.2.0 - 
  (c) Rafal Zajac <rzajac@gmail.com>, Valentin Hervieu <valentin@hervieu.me>, Jussi Saarivirta <jusasi@gmail.com>, Angelin Sirbu <angelin.sirbu@gmail.com> - 
- https://github.com/rzajac/angularjs-slider.git - 
- 2015-12-20 */
+ https://github.com/angular-slider/angularjs-slider - 
+ 2015-12-21 */
 /*jslint unparam: true */
 /*global angular: false, console: false, define, module */
 (function(root, factory) {
