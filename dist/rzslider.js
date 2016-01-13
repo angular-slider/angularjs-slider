6ca68cb358e24ca8403d065ed8ca71eec8de66b8
Elias Cohenca <elias.cohenca@autodesk.com>
2016-01-13 11:01:56 +0200

    perf(): Remove  inside onChange event
    When the callback run while changing the slider takes a certain amount of time (~50 ms or more), the
     inside onChange can stall resulting in many changes lost along the way while it catches up.
    Removing seems to have no visible side effects. It might be possible that this is due to
    scope.() being called right before callOnChange(), so the model is updated anyway (see in
    applyModel)

--- dist/rzslider.js
+++ dist/rzslider.js
@@ -1,7 +1,7 @@
 /*! angularjs-slider - v2.4.0 - 
  (c) Rafal Zajac <rzajac@gmail.com>, Valentin Hervieu <valentin@hervieu.me>, Jussi Saarivirta <jusasi@gmail.com>, Angelin Sirbu <angelin.sirbu@gmail.com> - 
  https://github.com/angular-slider/angularjs-slider - 
- 2015-12-30 */
+ 2016-01-13 */
 /*jslint unparam: true */
 /*global angular: false, console: false, define, module */
 (function(root, factory) {
@@ -812,10 +812,7 @@
        */
       callOnChange: function() {
         if (this.options.onChange) {
-          var self = this;
-          $timeout(function() {
-            self.options.onChange(self.options.id);
-          });
+          this.options.onChange(this.options.id);
         }
       },
 
