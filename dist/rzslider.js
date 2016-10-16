c8f22ecc7cbfc2e12d2082455cd83879ea629aed
Valentin Hervieu <valentin@hervi.eu>
2016-10-16 19:10:09 +0200

    5.7.0 release

--- dist/rzslider.js
+++ dist/rzslider.js
@@ -1,4 +1,4 @@
-/*! angularjs-slider - v5.6.0 - 
+/*! angularjs-slider - v5.7.0 - 
  (c) Rafal Zajac <rzajac@gmail.com>, Valentin Hervieu <valentin@hervieu.me>, Jussi Saarivirta <jusasi@gmail.com>, Angelin Sirbu <angelin.sirbu@gmail.com> - 
  https://github.com/angular-slider/angularjs-slider - 
  2016-10-16 */
