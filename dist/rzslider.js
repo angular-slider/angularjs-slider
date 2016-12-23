e156f84d02aab34195f4914692d30e63ea73e7d9
Valentin Hervieu <valentin@hervi.eu>
2016-12-23 12:22:30 +0100

    perf(css rules): Refactor the LESS file to flatten the generated rules
    Thanks to this refactoring, it's now easier to overwrite the style
    
    close #479

--- dist/rzslider.js
+++ dist/rzslider.js
@@ -1,7 +1,7 @@
 /*! angularjs-slider - v5.9.0 - 
  (c) Rafal Zajac <rzajac@gmail.com>, Valentin Hervieu <valentin@hervieu.me>, Jussi Saarivirta <jusasi@gmail.com>, Angelin Sirbu <angelin.sirbu@gmail.com> - 
  https://github.com/angular-slider/angularjs-slider - 
- 2016-12-12 */
+ 2017-01-02 */
 /*jslint unparam: true */
 /*global angular: false, console: false, define, module */
 (function(root, factory) {
