f37b9e525fe2d83b314f0a184c162a3641194f1d
Valentin Hervieu <valentin.hervieu@ricardo.ch>
2017-07-08 20:02:57 +0200

    6.2.3 release

--- dist/rzslider.js
+++ dist/rzslider.js
@@ -1,7 +1,7 @@
-/*! angularjs-slider - v6.2.2 - 
+/*! angularjs-slider - v6.2.3 - 
  (c) Rafal Zajac <rzajac@gmail.com>, Valentin Hervieu <valentin@hervieu.me>, Jussi Saarivirta <jusasi@gmail.com>, Angelin Sirbu <angelin.sirbu@gmail.com> - 
  https://github.com/angular-slider/angularjs-slider - 
- 2017-05-26 */
+ 2017-07-08 */
 /*jslint unparam: true */
 /*global angular: false, console: false, define, module */
 (function(root, factory) {
