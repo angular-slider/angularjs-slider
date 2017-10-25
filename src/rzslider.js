c7dd86fd523f2198e2767bd2465fe6652c8e5a37
Daan Reid <osmosisch@gmail.com>
2017-10-25 09:53:14 +0200

    Also update restrictedBar on slider reset

--- src/rzslider.js
+++ src/rzslider.js
@@ -641,6 +641,7 @@
           this.manageEventsBindings()
           this.setDisabledState()
           this.calcViewDimensions()
+          this.updateRestrictionBar()
           this.refocusPointerIfNeeded()
         },
 
