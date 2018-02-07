f8a5ebbfe0dda586059a106eb4b1fb4dcad380fb
Troy McKinnon <troy.t.mckinnon@gmail.com>
2018-02-07 17:33:39 -0600

    fix(Typescript Support): Add labelOverlapSeparator to d.ts

--- dist/rzslider.js
+++ dist/rzslider.js
@@ -1,7 +1,7 @@
 /*! angularjs-slider - v6.5.0 - 
  (c) Rafal Zajac <rzajac@gmail.com>, Valentin Hervieu <valentin@hervieu.me>, Jussi Saarivirta <jusasi@gmail.com>, Angelin Sirbu <angelin.sirbu@gmail.com> - 
  https://github.com/angular-slider/angularjs-slider - 
- 2018-02-06 */
+ 2018-02-07 */
 /*jslint unparam: true */
 /*global angular: false, console: false, define, module */
 ;(function(root, factory) {
