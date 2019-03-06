26620292da730a975a67bfd6429a36601cd6a61c
Troy McKinnon <troy.t.mckinnon@gmail.com>
2019-03-06 16:29:28 -0600

    fix(Typescript): typescript support for ticksArray as array of objects

--- dist/rzslider.js
+++ dist/rzslider.js
@@ -1,7 +1,7 @@
 /*! angularjs-slider - v7.0.0 - 
  (c) Rafal Zajac <rzajac@gmail.com>, Valentin Hervieu <valentin@hervi.eu>, Jussi Saarivirta <jusasi@gmail.com>, Angelin Sirbu <angelin.sirbu@gmail.com> - 
  https://github.com/angular-slider/angularjs-slider - 
- 2019-02-23 */
+ 2019-03-06 */
 /*jslint unparam: true */
 /*global angular: false, console: false, define, module */
 ;(function(root, factory) {
